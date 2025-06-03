3c40f82eb0eacf9581273b54736a95f9d47e4544
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-06-03 18:59:11 +0530

    Error solved

--- Backend/src/middlewares/error.middleware.js
+++ Backend/src/middlewares/error.middleware.js
@@ -1,4 +1,6 @@
 const errorHandler = (err, req, res, next) => {
+    console.log('statusCode:', err.statusCode);
+    console.log('Error caught:', err);
   res.status(err.statusCode || 500).json({
     success: err.success || false,
     message: err.message || "Internal Server Error",
--- Backend/src/utils/apierror.js
+++ Backend/src/utils/apierror.js
@@ -1,11 +1,11 @@
 class ApiError extends Error{
-    constructor(statusCode,message="Something went wrong",errors=[],stack=""){
+    constructor(code,message="Something went wrong",errors=[],stack=""){
         super(message);
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
+        this.code=code;
+        this.data=null;
+        this.message=message;
         this.success=false;
-        this.errors=errors
+        this.errors=errors;
         if(stack){
             this.stack=stack;
         }
