7d0b73fe9a3cc01f11d98ec03e5baf331dd35c81
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-06-04 18:30:04 +0530

    refresh error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base:"./",
   server: {
     proxy:{
       '/api': `http://localhost:${process.env.PORT || 8000}`
