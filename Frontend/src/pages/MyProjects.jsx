f976a7ded3daf8f1e3320ccd646e311f42651725
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-06-17 17:01:54 +0530

    Done

--- Frontend/src/pages/MyProjects.jsx
+++ Frontend/src/pages/MyProjects.jsx
@@ -84,7 +84,7 @@ export default function MyProjects() {
           transition={{ duration: 0.4 }}
           className="w-full max-w-7xl mx-auto py-6 flex-grow px-4"
         >
-          <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div className="flex flex-wrap justify-center gap-6">
             {renderContent()}
           </div>
         </motion.div>
