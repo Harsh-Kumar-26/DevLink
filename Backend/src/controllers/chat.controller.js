feab0729693240b42e5563216eb1ee6481774acb
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-07-05 19:26:45 +0530

    C3

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -1,6 +1,10 @@
 // Add to your routes (e.g., chat.routes.js)
 import express from "express";
 import Chat from "../models/chat.model.js";
+import { asynchandler } from "../utils/asynchandler.js";
+import ApiResponse from "../utils/apiresponse.js";
+import {ApiError} from "../utils/apierror.js"
+
 
 export const chatdata=asynchandler(async(req,res)=>{
     const {pjtid}=req.body;
